6ad0e38d6ff2b1185933aab397ffdd31e2cc3c38
Anoir Khemiri <anoir@Air-van-Anoir.home>
2021-03-15 22:47:04 +0100

    eerste versie

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const https = require("https");
+const body = require("body-parser")
+const app = express();
+
+
+app.use(body.urlencoded({extended: false}))
+
+let counter = 0;
+app.get("/", (req, res)=>{
+    counter +=1;
+
+    https.get("https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=ea468c722a4601f08806754e47e8d196", (anoir)=>{
+
+console.log(anoir.statusCode);
+anoir.on("data", (datainfo)=>{
+    const dataver = JSON.parse(datainfo)
+    let lijst = dataver.weather[0].main
+    
+
+    res.send(`het weer in london is:${lijst}`)
+})
+
+})
+
+
+   //res.send("<h1>ja wij zijn live</h1>" );
+
+
+
+console.log(`pageview is nu ${counter}`)
+    });
+
+app.listen(process.env.PORT || 80, ()=>{
+    console.log("server is aan het luisteren...");
+});
+
+
